93b601f3f83e8dd02e935c55130eb2587378a50d
angelali14 <angelal162@nycstudents.net>
2024-05-07 08:45:27 -0400

    Add files via upload
    beginning to code

---
+++ main.py
@@ -0,0 +1,17 @@
+import pygame
+
+pygame.init()
+pygame.font.init()
+my_font = pygame.font.SysFont('Arial', 15)
+pygame.display.set_caption("Welcome to TicTacTwisted")
+
+SCREEN_HEIGHT = 500
+SCREEN_WIDTH = 500
+size = (SCREEN_WIDTH, SCREEN_HEIGHT)
+screen = pygame.display.set_mode(size)
+title_screen = pygame.display.set_mode(size)
+
+
+
+
+
