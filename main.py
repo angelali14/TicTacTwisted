2a909c4d124f80acbbedeb991d4d4895f3e70610
angelali14 <angelal162@nycstudents.net>
2024-05-10 08:39:06 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -9,29 +9,31 @@ SCREEN_HEIGHT = 500
 SCREEN_WIDTH = 500
 size = (SCREEN_WIDTH, SCREEN_HEIGHT)
 screen = pygame.display.set_mode(size)
-title_screen = pygame.display.set_mode(size)
 
-r = 50
-g = 0
-b = 100
+r = 255
+g = 182
+b = 193
 
 run = True
 is_player1_turn = True
 is_player2_turn = False
 
 message = "Click an Option to Start!"
-option1 = "| play with bot |"
-option2 = "| play with friends |"
+
 display_message = my_font.render(message, True, (255, 255, 255))
-display_option1 = my_font.render(option1, True, (255, 255, 255))
-display_option2 = my_font.render(option2, True, (255, 255, 255))
+
 
 # -------- Main Program Loop -----------
 ## ----- NO BLIT ZONE START ----- ##
 while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+           run = False
+    pygame.display.update()
+
+pygame.quit()
 
 
 ##  ----- NO BLIT ZONE END  ----- ##
-
 screen.fill((r, g, b))
 screen.blit(display_message, (0, 0))
