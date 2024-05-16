5c18aed52aa6d1901d98920a5f771906f3d5064a
angelali14 <angelal162@nycstudents.net>
2024-05-16 08:38:51 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,35 +1,54 @@
 import pygame
+from start import Start
+from exit import Exit
+import random
 
 pygame.init()
 pygame.font.init()
 my_font = pygame.font.SysFont('Arial', 15)
-pygame.display.set_caption("Welcome to TicTacTwisted")
+pygame.display.set_caption("TicTacTwisted")
 
-SCREEN_HEIGHT = 500
-SCREEN_WIDTH = 500
+# set up variables for the display
+SCREEN_HEIGHT = 370
+SCREEN_WIDTH = 530
 size = (SCREEN_WIDTH, SCREEN_HEIGHT)
 screen = pygame.display.set_mode(size)
 
-r = 255
-g = 182
-b = 193
+r = 50
+g = 0
+b = 100
+clicks = 0 
+
+message = "Welcome to TicTacTwisted!"
+display_message = my_font.render(message, True, (255, 255, 255))
+display_clicks = my_font.render(str(clicks), True, (255, 255, 255))
+
+s = Start(40, 40)
+e = Exit(100, 40)
 
 run = True
-
-start_img = pygame.image.load('start.png').convert_alpha()
-exit_img = pygame.image.load('exit.png').convert_alpha()
-
 
 # -------- Main Program Loop -----------
 ## ----- NO BLIT ZONE START ----- ##
 while run:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-           run = False
-    pygame.display.update()
+    for event in pygame.event.get():  # User did something
+        if event.type == pygame.QUIT:  # If user clicked close
+            run = False
+        if event.type == pygame.MOUSEBUTTONUP:
+            if s.rect.collidepoint(event.pos):
+                x = random.randint(0, 370)
+                y = random.randint(0, 270)
+                s.x = x
+                s.y = y
+                s.move(s.x, s.y)
+                clicks = clicks + 1
+
 
 pygame.quit()
 
-
 ##  ----- NO BLIT ZONE END  ----- ##
 screen.fill((r, g, b))
+screen.blit(display_message, (0, 0))
+screen.blit(s.image, s.rect)
+pygame.display.update()
+screen.blit(display_clicks, (20, 40))
