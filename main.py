a424494cb0437f2fce4b8338a7592715d5cb638a
angelali14 <angelal162@nycstudents.net>
2024-05-21 08:43:15 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import pygame
-from start import Start
 import time
 
 # set up pygame modules
@@ -8,6 +7,20 @@ pygame.font.init()
 my_font = pygame.font.SysFont('Arial', 15)
 pygame.display.set_caption("TicTacTwisted")
 
+start_img = pygame.image.load('start.png')
+start_img = pygame.transform.scale(start_img, (250, 150))
+exit_img = pygame.image.load('exit.png')
+exit_img = pygame.transform.scale(exit_img, (300, 140))
+class Button():
+    def __init__(self, x, y, image):
+        self.image =image
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x,y)
+
+    def draw(self):
+        screen.blit(self.image, (self.rect.x, self.rect.y))
+
+
 # set up variables for the display
 SCREEN_HEIGHT = 500
 SCREEN_WIDTH = 600
@@ -15,8 +28,11 @@ size = (SCREEN_WIDTH, SCREEN_HEIGHT)
 screen = pygame.display.set_mode(size)
 title_screen = pygame.display.set_mode(size)
 
-current_time = time.time()
-start_time = current_time + 10
+start_button = Button(50, 200, start_img)
+exit_button = Button(300, 205, exit_img)
+
+# current_time = time.time()
+# start_time = current_time + 10
 
 
 name = "Collect coins as fast as you can!"
@@ -25,14 +41,6 @@ r = 50
 g = 0
 b = 100
 
-# render the text for later
-display_name = my_font.render(name, True, (255, 255, 255))
-display_message = my_font.render(message, True, (255, 255, 255))
-
-
-score = 0
-s = Start(100, 100)
-
 # The loop will carry on until the user exits the game (e.g. clicks the close button).
 run = True
 intro = True
@@ -40,7 +48,9 @@ game_over = False
 # -------- Main Program Loop -----------
 while run:
     screen.fill((245, 195, 194))
-    s =
+
+    start_button.draw()
+    exit_button.draw()
 
     # --- Main event loop
     for event in pygame.event.get():  # User did something
@@ -52,6 +62,4 @@ while run:
 pygame.quit()
 
 screen.fill((r, g, b))
-screen.blit(display_message, (0, 0))
-screen.blit(s.image, s.rect)
 pygame.display.update()
