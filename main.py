5dea0a77ada61dead1f763eaf52b1d62edf0e67c
angelali14 <angelal162@nycstudents.net>
2024-05-28 08:40:03 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -16,8 +16,18 @@ class Button():
         self.image =image
         self.rect = self.image.get_rect()
         self.rect.topleft = (x,y)
+        self.clicked = False
 
     def draw(self):
+        pos = pygame.mouse.get_pos()
+        if self.rect.collidepoint(pos):
+            if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
+                self.clicked = True
+                print("CLICKED")
+
+        if pygame.mouse.get_pressed()[0] == 0:
+            self.clicked = False
+
         screen.blit(self.image, (self.rect.x, self.rect.y))
 
 
